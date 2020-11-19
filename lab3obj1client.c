0728cf1294138f910431bdfebcc1721a241cd980
Atish Mohapatra <69408821+Atish1998@users.noreply.github.com>
2020-11-19 12:01:27 +0530

    Create lab3obj1client.c
    A) TO IMPLEMENT A PROGRAM FOR AN ECHO CLIENT- SERVER USING UDP

---
+++ lab3obj1client.c
@@ -0,0 +1,33 @@
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<unistd.h>
+#include<netdb.h>
+#include<stdio.h>
+#include<string.h>
+#include<arpa/inet.h>
+#define MAXLINE 1024
+int main(int argc, char **argv)
+{
+int sockfd;
+int n;
+socklen_t len;
+char msg[1024];
+struct sockaddr_in servaddr,cliaddr;
+sockfd=socket(AF_INET,SOCK_DGRAM,0);
+//bzero(&servaddr,sizeof(servaddr));
+servaddr.sin_family=AF_INET;
+servaddr.sin_addr.s_addr=INADDR_ANY;
+servaddr.sin_port=htons(5035);
+printf("\n\nBinded");
+bind(sockfd,(struct sockaddr*)&servaddr,sizeof(servaddr));
+printf("\n\nListening");
+printf("\n");
+len=sizeof(cliaddr);
+n=recvfrom(sockfd,msg,MAXLINE,0,(struct sockaddr*)&cliaddr,&len);
+printf("\n clients message: %s\n",msg);
+if(n<6)
+perror("send error");
+sendto(sockfd,msg,n,0,(struct sockaddr*)&cliaddr,len);
+return 0;
+}
